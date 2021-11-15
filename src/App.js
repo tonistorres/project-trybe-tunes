a4c0c18701b9767955a46f9d1d0f9fe57384a718
tonistorres <sisvenda2011@gmail.com>
2021-11-15 18:01:04 -0300

    feat:Qt-01 Respondida - Implementando Componentes Criando Rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Seach';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <p>TrybeTunes</p>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/profile/edit" component={ ProfileEdit } />
+            <Route exact path="/profile" component={ Profile } />
+            <Route path="/favorites" component={ Favorites } />
+            <Route path="/album/:id" component={ Album } />
+            <Route path="/search" component={ Search } />
+            <Route exact path="/" component={ Login } />
+            <Route exact path="*" component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PageNotFound extends Component {
+  render() {
+    return (
+      <div data-testid="page-not-found"> ERRO 404 </div>
+    );
+  }
+}
+
+export default PageNotFound;
