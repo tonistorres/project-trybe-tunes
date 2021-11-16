8fb99e5535fab12af8f6e86174c369a9b45c1b92
tonistorres <sisvenda2011@gmail.com>
2021-11-16 12:03:07 -0300

    chore: Correcao lint

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../Componentes/Header';
 
 class Album extends Component {
   render() {
-    const { match } = props;
+    const { match: { params: { id } } } = this.props;
     return (
       <div data-testid="page-album">
         <Header />
-        <h1>{match.id}</h1>
+        <h1>{id}</h1>
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+}.isRequired;
 
 export default Album;
